66f3d83b93cdad7af6aaa37a52ce87dd5c4012b2
phoebewalquist <pheebijane@yahoo.com>
2023-05-10 08:02:02 -0700

    Initial commit

---
+++ controllers/flights.js
@@ -0,0 +1,48 @@
+const Flight = require('../models/flight');
+
+
+module.exports = {
+  index,
+  new: newFlight,
+  create
+};
+
+
+function index(req, res) {
+  Flight.find({})
+    .then((flights) => {
+      res.render('flights/index', {
+        flights,
+        title: 'All Flights',
+        dateFormat: (date) => {
+          return new Date(date).toLocaleString();
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/');
+    });
+}
+
+function newFlight(req, res) {
+  res.render('flights/new', {
+    title: 'Add Flight', 
+    errorMsg: '' 
+  });
+}
+
+
+async function create(req, res) {
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key];
+  }
+
+  try {
+    await Flight.create(req.body);
+    res.redirect('/flights');
+  } catch (err) {
+    console.log(err);
+    res.render('flights/new', { errorMsg: err.message });
+  }
+}
